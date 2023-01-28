eda29405bb863655880ce143f94eb3d51ad1be03
Aleksandr Slesarev <slessarevalex@ya.ru>
2023-01-28 13:19:42 +0300

    Задал размер массива и предложил пользователю ввести его значения

--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
 ﻿// Написать программу, которая из имещегося массива строк, формирует массив из строк, длина которых меньше либо равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на страте выполнения алгоритма.
 
+int EnterData(string text) //Ввод размера массива
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+string[] FillArray(int n) //Заполнение массива
+{
+    string[] array = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите {i}-й элемент массива: ");
+            array[i] = Console.ReadLine();
+        }
+    return array;
+}
+
+int arraySize = EnterData("Введите размер массива");
+string[] array = FillArray(arraySize);
+for (int i = 0; i < arraySize; i++)
+{
+    Console.WriteLine(array[i]);    
+}
+
